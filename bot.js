cc057f2716f95876b5caa20410e904d22a731ad4
Daniel Andersson <d.andersson92@gmail.com>
2016-04-22 08:03:41 +0200

    Changed to a echo bot
    Changed the bot behavior into a echo bot instead of evaluating the
    message as JS.

--- bot.js
+++ bot.js
@@ -1,21 +1,8 @@
 'use strict';
-
-const safeEval = require('safe-eval');
-
 function echo(event) {
-  return new Promise((resolve, reject) => {
-    let result;
-    try {
-      result = safeEval(event.message.text);
-    } catch (error) {
-      result = error.toString();
-    }
-
-    console.log(`Evaluated ${event.message.text} to ${result}.`);
-    resolve({
-      messageText: result,
-      senderId: event.sender.id
-    });
+  return Promise.resolve({
+    messageText: event.message.text,
+    senderId: event.sender.id
   });
 }
 
